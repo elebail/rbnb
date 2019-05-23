c8bcb75c0106a75a45f136d507a1ce5d3d195aeb
Victor Authiat <authiatv@gmail.com>
2019-05-23 18:05:31 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,2 +1,41 @@
 class SpaceshipsController < ApplicationController
+def index
+    @spaceship = Spaceship.all
+  end
+
+  def indexx
+    @spaceship = Spaceship.all
+  end
+
+  def show
+    @spaceship = Spaceship.find(params[:id])
+  end
+
+  def new
+    @spaceship = Spaceship.new
+  end
+
+  def create
+
+    @spaceship = Spaceship.new(set_spaceship)
+    @spaceship.user_id = current_user.id
+    if @spaceship.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+        @spaceship = Spaceship.find(params[:id])
+
+    @spaceship.destroy
+    redirect_to spaceship_path
+  end
+
+  private
+
+  def set_spaceship
+    params.require(:spaceship).permit(:name, :price, :photo)
+  end
 end
