e2928da538cf17e8c0a01f960c17ce4e4007bcd8
Emeric Le Bail <emeric.lebail@gmail.com>
2019-05-23 18:20:30 +0200

    created the spaceship show

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,2 +1,11 @@
 class SpaceshipsController < ApplicationController
+  def show
+    @spaceship = Spaceship.find(params[:id])
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:spaceship).permit(:name, :photo, :price)
+  end
 end
