54774e0931127763be25b5fd06ae49a2a913fc19
Emeric Le Bail <emeric.lebail@gmail.com>
2019-05-28 11:50:31 +0200

    navabar upadted, fixed, container width fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import flatpickr from 'flatpickr';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
 import 'flatpickr/dist/flatpickr.min.css';
-flatpickr(".datepicker",{
-});
+flatpickr(".datepicker",{});
 
