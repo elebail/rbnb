78c8a70958d772465a58b83c3aa282a94b8a849d
Emeric Le Bail <emeric.lebail@gmail.com>
2019-05-29 13:28:14 +0200

    dashboard done, orbitron bold added, .red class added to rental status on dashboard.scsss

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,8 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @pending_rentals = current_user.rental_requests.where(accepted: nil)
+    @accepted_rentals = current_user.rental_requests.where(accepted: true)
+    @declined_rentals = current_user.rental_requests.where(accepted: false)
   end
 end
